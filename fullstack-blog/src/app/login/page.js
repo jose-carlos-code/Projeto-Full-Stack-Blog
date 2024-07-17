adae87ffb6e0a6065be0ba3ba6145ccd3e94627b
josé carlos <josecampos2030.jdk@gmail.com>
2024-07-17 16:28:37 -0300

    update

---
+++ fullstack-blog/src/app/login/page.js
@@ -0,0 +1,9 @@
+export default function Login() {
+
+    return (
+      <main className="bg-black flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="bg-gray-400">PÁGINA DE LOGIN</h1>
+      </main>
+    );
+  }
+  
--- fullstack-blog/src/app/page.js
+++ fullstack-blog/src/app/page.js
@@ -1,9 +1,30 @@
-import Image from "next/image";
+// const handleLogin = () =>{
+//   const routes = useRouter();
+//   routes.push('/login');
+// }
+
+import { Router } from "express";
+const express = require('express');
+const app = express();
+
+const routes = Router();
+
+const rotaLogin = () => {
+  routes.get('/login', () => {
+    console.log('ROTA DE LOGIN EXECUTADA COM SUCESSO!');
+  });
+ 
+}
+
+
+app.use(routes);
 
 export default function Home() {
+
   return (
     <main className="bg-neutral-700 flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="bg-gray-400">HELLO WORLD</h1>
+      <a href="/login" onClick={ rotaLogin() } className="bg-blue-700">LOGIN</a>
     </main>
   );
 }

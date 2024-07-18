bf5e758010dc9b84d7ac58405c2186245bb5f950
josé carlos <josecampos2030.jdk@gmail.com>
2024-07-18 17:00:37 -0300

    aa

--- API-NODE/src/controllers/GayController.js
+++ API-NODE/src/controllers/GayController.js
@@ -1,4 +1,6 @@
 exports.GayDa = (req, res) => {
-    let gayId = req.params.id;
-    return res.json( {'idGay': gayId, 'nomedo gay': 'Artur'} );
+    let gayId = req.body.id;
+    if(gayId === 3){
+        return res.json( {'idGay': gayId, 'nomedo gay': 'Artur'} );
+    }
 }
--- fullstack-blog/src/app/page.js
+++ fullstack-blog/src/app/page.js
@@ -24,6 +24,8 @@ export default function Home() {
   return (
     <main className="bg-neutral-700 flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="bg-gray-400">HELLO WORLD</h1>
+      <label>NOME:</label>
+      <input placeholder="digite seu nome"></input>
       <a href="/login" onClick={ rotaLogin() } className="bg-blue-700">LOGIN</a>
     </main>
   );
